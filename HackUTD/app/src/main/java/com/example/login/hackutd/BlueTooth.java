03367973154f141964d5e10df6fda34e24f7bae2
Nhi Luong <nhi.luong@mavs.uta.edu>
2018-02-24 18:05:36 -0600

    bluetooth stuff

---
+++ HackUTD/app/src/main/java/com/example/login/hackutd/BlueTooth.java
@@ -0,0 +1,152 @@
+package com.example.login.hackutd;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class BlueTooth extends AppCompatActivity {
+    String LOG_TAG; // Just for logging purposes. Could be anything. Set to app_name
+    int REQUEST_ENABLE_BT = 99; // Any positive integer should work.
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothDevice BTDevice;
+    Button enableBT;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        enableBT = (Button)findViewById(R.id.enableBT);
+        enableBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableBluetoothOnDevice();
+            }
+        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_tooth);
+
+        LOG_TAG = getResources().getString(R.string.app_name);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+    private void enableBluetoothOnDevice()
+    {
+        if (mBluetoothAdapter == null)
+        {
+            Log.e(LOG_TAG, "This device does not have a bluetooth adapter");
+            finish();
+            // If the android device does not have bluetooth, just return and get out.
+            // There's nothing the app can do in this case. Closing app.
+        }
+
+        // Check to see if bluetooth is enabled. Prompt to enable it
+        if( !mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_ENABLE_BT)
+        {
+            if (resultCode == 0)
+            {
+                // If the resultCode is 0, the user selected "No" when prompt to
+                // allow the app to enable bluetooth.
+                // You may want to display a dialog explaining what would happen if
+                // the user doesn't enable bluetooth.
+                Toast.makeText(this, "The user decided to deny bluetooth access", Toast.LENGTH_LONG).show();
+            }
+            else
+                Log.i(LOG_TAG, "User allowed bluetooth access!");
+        }
+    }
+
+    private BluetoothObject getBluetoothDevices()
+    {
+        BluetoothObject BTDevice = null;
+
+        // Query paired devices
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are any paired devices
+        if (pairedDevices.size() > 0)
+        {
+            BTDevice = new BluetoothObject();
+
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices)
+            {
+                // Create the device object and add it to the arrayList of devices
+                BluetoothObject bluetoothObject = new BluetoothObject();
+                bluetoothObject.setBluetooth_name(device.getName());
+                bluetoothObject.setBluetooth_address(device.getAddress());
+                bluetoothObject.setBluetooth_state(device.getBondState());
+                bluetoothObject.setBluetooth_type(device.getType());    // requires API 18 or higher
+                //bluetoothObject.setBluetooth_uuids(device.getUuids());
+
+            }
+        }
+
+        return BTDevice;
+    }
+    private void displayFoundDevice()
+    {
+
+        // start looking for bluetooth devices
+        mBluetoothAdapter.startDiscovery();
+
+        // Discover new devices
+        // Create a BroadcastReceiver for ACTION_FOUND
+        final BroadcastReceiver mReceiver = new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, Intent intent)
+            {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action))
+                {
+                    // Get the bluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                    // Get the "RSSI" to get the signal strength as integer,
+                    // but should be displayed in "dBm" units
+                    int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+
+                    // Create the device object and add it to the arrayList of devices
+                    BluetoothObject bluetoothObject = new BluetoothObject();
+                    bluetoothObject.setBluetooth_name(device.getName());
+                    bluetoothObject.setBluetooth_address(device.getAddress());
+                    bluetoothObject.setBluetooth_state(device.getBondState());
+                    bluetoothObject.setBluetooth_type(device.getType());    // requires API 18 or higher
+                    //bluetoothObject.setBluetooth_uuids(device.getUuids());
+                    bluetoothObject.setBluetooth_rssi(rssi);
+
+                    // 1. Pass context and data to the custom adapter
+                    //FoundBTDevicesAdapter adapter = new FoundBTDevicesAdapter(getApplicationContext(), arrayOfFoundBTDevices);
+
+                    // 2. setListAdapter
+                    //setListAdapter(adapter);
+                }
+            }
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+    }
+}
